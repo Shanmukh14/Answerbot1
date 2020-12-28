f2717d772ecd91edc7cc6f472dc6fe1035e8a562
Shanmukh14 <76694157+Shanmukh14@users.noreply.github.com>
2020-12-28 20:45:42 +0530

    Update crowd.py

--- crowd.py
+++ crowd.py
@@ -10,7 +10,7 @@ import threading
 import concurrent
 
 BOT_OWNER_ROLE = 'RUNNER' # change to what you need
-#BOT_OWNER_ROLE_ID = "787948023165747230" 
+#BOT_OWNER_ROLE_ID = "793133876587266069" 
   
  
 
@@ -297,8 +297,8 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start) (' NzkzMTIyNDMyOTA3MDgzODI3.X-nq0w.u8jLBC2Pkmg2Lyh35vv2iH2Y6_0 
-                               ')
+    loop.create_task(bot.start (' NzkzMTIyNDMyOTA3MDgzODI3.X-nq0w.u8jLBC2Pkmg2Lyh35vv2iH2Y6_0 
+                               '))
                                
 
     loop.run_forever()
@@ -309,7 +309,7 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start('NzE5MDI4MjE3ODAwODE4ODUx.X-nbGQ.o0w3F35OMJ_RRusGc9_SRBlu6Gc ',
+    loop.create_task(selfbot.start('NzE5MDI4MjE3ODAwODE4ODUx.X-nbGQ.o0w3F35OMJ_RRusGc9_SRBlu6Gc',
                                    bot=False))
     loop.run_forever()
 
