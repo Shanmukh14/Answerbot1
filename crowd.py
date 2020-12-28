8f3b6e821030c8f556e36be4ef8116fe33197870
Shanmukh14 <76694157+Shanmukh14@users.noreply.github.com>
2020-12-28 21:12:03 +0530

    Update crowd.py

--- crowd.py
+++ crowd.py
@@ -309,8 +309,8 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start ('NzE5MDI4MjE3ODAwODE4ODUx.X-nbGQ.o0w3F35OMJ_RRusGc9_SRBlu6Gc,
-                                   bot=False'))
+    loop.create_task(selfbot.start ('NzE5MDI4MjE3ODAwODE4ODUx.X-nbGQ.o0w3F35OMJ_RRusGc9_SRBlu6Gc',
+                                   bot=False))
     loop.run_forever()
 
 if __name__ == '__main__':
