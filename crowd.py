f4c3e5fd2f9bc0005358a18e8b80969725624ae5
Shanmukh14 <76694157+Shanmukh14@users.noreply.github.com>
2020-12-28 20:25:50 +0530

    Update crowd.py

--- crowd.py
+++ crowd.py
@@ -297,8 +297,8 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start('NzkzMTIyNDMyOTA3MDgzODI3.X-nq0w.u8jLBC2Pkmg2Lyh35vv2iH2Y6_0
-'))
+    loop.create_task(bot.start('NzkzMTIyNDMyOTA3MDgzODI3.X-nq0w.u8jLBC2Pkmg2Lyh35vv2iH2Y6_0'))
+
     loop.run_forever()
 
 
